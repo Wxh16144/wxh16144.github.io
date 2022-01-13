c74a0fd6a3bf37ea560a62d8fb3b4ae21aafcebe
Wuxh <wxh1220@gmail.com>
2022-01-13 22:26:04 +0800

    chore: remove about

--- scripts/generator_index.js
+++ scripts/generator_index.js
@@ -10,9 +10,14 @@ const files = [
     routePath: 'bookmarks/index.html',
     data: { URL: './README' },
   },
+  {
+    templatePath,
+    routePath: 'about/index.html',
+    data: { URL: 'https://github.com/wxh16144/wxh16144' }
+  }
 ];
 
-hexo.extend.filter.register('after_generate',async () => {
+hexo.extend.filter.register('after_generate', async () => {
   for (const { templatePath, routePath, data } of files) {
     const code = await hexo.render.render({ path: templatePath }, data)
     hexo.route.set(routePath, `${code}`)
